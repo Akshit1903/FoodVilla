08dbefe9c718300e10c5bbcbbe675f20db139314
Akshit Lalit <akshit.lalit@gmail.com>
2023-12-15 17:30:23 +0530

    Updates

---
+++ Instamart.d9130f4c.js
@@ -0,0 +1,3 @@
+var e=globalThis.parcelRequire1a02;(0,e.register)("5KNkF",function(s,a){Object.defineProperty(s.exports,"__esModule",{value:!0}),s.exports.default=void 0;var l=e("ayMG0");s.exports.default=()=>/*#__PURE__*/(0,l.jsxs)("div",{className:"m-4 p-1 flex flex-col min-h-[70vh]",children:[/*#__PURE__*/(0,l.jsx)("h1",{className:"text-lg font-bold",children:"Instamart"}),/*#__PURE__*/(0,l.jsx)("h3",{children:"Coming soon! (This Component is loaded using Lazy Loading)"})]})});//# sourceMappingURL=Instamart.d9130f4c.js.map
+
+//# sourceMappingURL=Instamart.d9130f4c.js.map
---
+++ Profile.8d9f22ae.js
@@ -0,0 +1,3 @@
+var e=globalThis.parcelRequire1a02;(0,e.register)("aoYtd",function(r,t){Object.defineProperty(r.exports,"__esModule",{value:!0}),r.exports.default=void 0;var a,d=(a=e("acw62"))&&a.__esModule?a:{default:a},l=e("ayMG0");class o extends d.default.Component{render(){return/*#__PURE__*/(0,l.jsx)("h1",{children:"Easter Egg"})}}r.exports.default=o});//# sourceMappingURL=Profile.8d9f22ae.js.map
+
+//# sourceMappingURL=Profile.8d9f22ae.js.map
